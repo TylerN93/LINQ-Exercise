abc75dcfb2d420062a97c4477eb619d3b704f4ee
TylerN93 <tylernewton73@gmail.com>
2022-11-08 00:03:07 -0600

    Completed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace LINQ
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var games = new List <string> () { "Madden", "NBA", "Call Of Duty", "Fortnight" };
+
+            //var order = games.OrderBy();
+
+            games = games.OrderBy(g => g.Length).ToList();
+
+            for (int i = 0; i< games.Count; i++)
+
+            {
+                Console.WriteLine($"{games[i]}");
+            }
+            
         }
     }
 }
